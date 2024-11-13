aa44c3d4ce9b13359ab722573ad317f477539884
PhamKhac <phamkhac2003@gmail.com>
2024-11-13 15:20:22 +0700

    update Admin

--- FrontEnd/src/Components/Administrator/Sidebar.jsx
+++ FrontEnd/src/Components/Administrator/Sidebar.jsx
@@ -208,8 +208,8 @@ const Sidebar = () => {
         <button className="rounded-circle border-0" id="sidebarToggle"></button>
       </div> */}
 
-      <div class="sidebar-card d-none d-lg-flex mt-8">
-                <img class="sidebar-card-illustration mb-2" src="/src/assets/images/undraw_rocket.svg" alt="..."/>
+      <div className="sidebar-card d-none d-lg-flex mt-8">
+                <img className="sidebar-card-illustration mb-2" src="/src/assets/images/undraw_rocket.svg" alt="..."/>
                 {/* <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
                 <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a> */}
             </div>
