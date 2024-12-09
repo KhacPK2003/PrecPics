4317d4d42af7ac6005d3271908c597d337d6349e
cuuthiennn <thienweret@gmail.com>
2024-12-09 08:02:43 +0700

    change fuzzy query to match query with fuzziness

--- BackEnd/src/main/java/com/example/prepics/utils/ElasticSearchUtil.java
+++ BackEnd/src/main/java/com/example/prepics/utils/ElasticSearchUtil.java
@@ -1,20 +1,20 @@
 package com.example.prepics.utils;
 
-import co.elastic.clients.elasticsearch._types.query_dsl.FuzzyQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
-import lombok.val;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
+import co. elastic. clients. elasticsearch._types. query_dsl. Operator;
 
 import java.util.function.Supplier;
 
 public class ElasticSearchUtil {
 
     public static Supplier<Query> createSupplierQuery(String fieldName, String approximate){
-        Supplier<Query> supplier = ()->Query.of(q->q.fuzzy(createFuzzyQuery(fieldName, approximate)));
-        return  supplier;
+        return ()->Query.of(q->q.match(createFuzzyQuery(fieldName, approximate)));
     }
 
-    public static FuzzyQuery createFuzzyQuery(String fieldName, String approximate){
-        val fuzzyQuery  = new FuzzyQuery.Builder();
-        return  fuzzyQuery.field(fieldName).value(approximate).fuzziness("3").build();
+    public static MatchQuery createFuzzyQuery(String fieldName, String approximate){
+        return QueryBuilders.match().field(fieldName).query(approximate)
+                .operator(Operator.And).fuzziness("auto").build();
     }
 }
